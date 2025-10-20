cb8be802b06f67f6ffed116894b76cdb3a5fd1b8
Davyd19 <davydyehuda196@gmail.com>
2025-10-20 21:25:37 +0700

    Fix: Remove unused import in ShoppingList.kt
    This commit removes the unused `androidx.compose.runtime.*` wildcard import from the `ShoppingList.kt` file.

--- app/src/main/java/com/example/shoppinglist/components/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/components/ShoppingList.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
